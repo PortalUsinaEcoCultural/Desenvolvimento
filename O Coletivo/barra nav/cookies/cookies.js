114cbbef995099ede913a5b421ca4e4eda62e633
Let <leticiacarvalhosilva2006@gmail.com>
2024-11-06 10:06:05 -0300

    Cookies

--- O Coletivo/barra nav/cookies/cookies.js
+++ O Coletivo/barra nav/cookies/cookies.js
@@ -1,11 +1,18 @@
+//ao clicar o botão de rejeitar, ele chama essa função
 function aceitarCookies() {
     document.getElementById('cookies-aviso').style.display = 'none';
-    // Salva a preferência do usuário no localStorage para futuras visitas
     localStorage.setItem('cookiesAceitos', 'true');
 }
 
+//ao clicar o botão de rejeitar, ele chama essa função
 function rejeitarCookies() {
     document.getElementById('cookies-aviso').style.display = 'none';
-    // Salva a preferência do usuário no localStorage para futuras visitas
     localStorage.setItem('cookiesAceitos', 'false');
+}
+
+//Para ver se o usuário ja aceitou o cookie alguma vez
+window.onload = function() {
+    if (localStorage.getItem('cookiesAceitos') !== null) {
+        document.getElementById('cookies-aviso').style.display = 'none';
+    }
 }
