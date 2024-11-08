2ab9494f193728431bf8a1701845214aab23cbd0
mateus martins <24.00553-3@maua.br>
2024-11-08 14:06:19 -0300

    Quem Somos: Parceiros e Conexões
    Adicionei um novo carrossel

--- Quem Somos/script.js
+++ Quem Somos/script.js
@@ -59,3 +59,49 @@ const appearOnScroll = new IntersectionObserver((entries, observer) => {
 faders.forEach(fader => {
   appearOnScroll.observe(fader);
 });
+
+
+// JavaScript para a nova seção "Parceiros e Conexões"
+const carrosselConexoes = document.querySelector(".carrossel-conexoes");
+const arrowBtnsConexoes = document.querySelectorAll(".wrapper-conexoes i");
+const firstParceiroWidthConexoes = carrosselConexoes.querySelector(".parceiro-conexoes").offsetWidth;
+const carrosselChildrenConexoes = [...carrosselConexoes.children];
+
+let isDraggingConexoes = false, startXConexoes, startScrollLeftConexoes;
+let parceirosPerViewConexoes = Math.round(carrosselConexoes.offsetWidth / firstParceiroWidthConexoes);
+
+carrosselChildrenConexoes.slice(-parceirosPerViewConexoes).reverse().forEach(parceiro => {
+    carrosselConexoes.insertAdjacentHTML("afterbegin", parceiro.outerHTML);
+});
+
+carrosselChildrenConexoes.slice(0, parceirosPerViewConexoes).forEach(parceiro => {
+    carrosselConexoes.insertAdjacentHTML("beforeend", parceiro.outerHTML);
+});
+
+arrowBtnsConexoes.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carrosselConexoes.scrollLeft += btn.id === "left-conexoes" ? -firstParceiroWidthConexoes : firstParceiroWidthConexoes;
+    });
+});
+
+const dragStartConexoes = (e) => {
+    isDraggingConexoes = true;
+    carrosselConexoes.classList.add("dragging");
+    startXConexoes = e.pageX;
+    startScrollLeftConexoes = carrosselConexoes.scrollLeft;
+};
+
+const draggingConexoes = (e) => {
+    if (!isDraggingConexoes) return;
+    e.preventDefault();
+    carrosselConexoes.scrollLeft = startScrollLeftConexoes - (e.pageX - startXConexoes);
+};
+
+const dragStopConexoes = () => {
+    isDraggingConexoes = false;
+    carrosselConexoes.classList.remove("dragging");
+};
+
+carrosselConexoes.addEventListener("mousedown", dragStartConexoes);
+carrosselConexoes.addEventListener("mousemove", draggingConexoes);
+document.addEventListener("mouseup", dragStopConexoes);
