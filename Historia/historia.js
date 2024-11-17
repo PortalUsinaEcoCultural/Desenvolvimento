74fc0a9258ba5e3aef2b56ee684e6ddc3603b97e
mateus martins <24.00553-3@maua.br>
2024-11-17 12:29:06 -0300

    História: Apoiadores
    criei uma duas novas partes, ret2 e Apoiadores; o carrosel está funcionando; ajustei uns espaçamentos

---
+++ Historia/historia.js
@@ -0,0 +1,45 @@
+/* Seção "Apoiadores" */
+const carrossel = document.querySelector(".carrossel");
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const firstParceiroWidth = carrossel.querySelector(".parceiro").offsetWidth;
+const carrosselChildren = [...carrossel.children];
+
+let isDragging = false, startX, startScrollLeft;
+
+let parceirosPerView = Math.round(carrossel.offsetWidth / firstParceiroWidth);
+
+carrosselChildren.slice(-parceirosPerView).reverse().forEach(parceiro => {
+    carrossel.insertAdjacentHTML("afterbegin", parceiro.outerHTML);
+});
+
+carrosselChildren.slice(0, parceirosPerView).forEach(parceiro => {
+    carrossel.insertAdjacentHTML("beforeend", parceiro.outerHTML);
+});
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carrossel.scrollLeft += btn.id === "left" ? -firstParceiroWidth : firstParceiroWidth;
+    });
+});
+
+const dragStart = (e) => {
+    isDragging = true;
+    carrossel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carrossel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    carrossel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carrossel.classList.remove("dragging");
+}
+
+carrossel.addEventListener("mousedown", dragStart);
+carrossel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
