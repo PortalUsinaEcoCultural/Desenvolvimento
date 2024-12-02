3334371fc7b1a3da33ae1a23a63f6100b2308ef9
Lyssa Okawa Perini <lyoperini2@gmail.com>
2024-12-02 00:30:19 -0300

    Aba Galeria: expansão das fotos
    consegui fazer as fotos serem expandidas quando clicadas!!

--- Javascript/AbaGaleria.js
+++ Javascript/AbaGaleria.js
@@ -13,3 +13,20 @@ function closeModal() {
     const modal = document.getElementById("imageModal");
     modal.style.display = "none";
 }
+
+<script>
+    function expandImage(img) {
+        const modal = document.getElementById("imageModal");
+        const modalImg = document.getElementById("expandedImage");
+        const captionText = document.getElementById("caption");
+
+        modal.style.display = "block";
+        modalImg.src = img.src;
+        captionText.innerHTML = img.alt;
+    }
+
+    function closeModal() {
+        const modal = document.getElementById("imageModal");
+        modal.style.display = "none";
+    }
+</script>
